4ca02be2ccb72758c538033821ba2b0da3de6284
Rayhan <prog.rayhan1@gmail.com>
2023-05-24 09:57:35 -0700

    refactor: add request length validation
    1.check query params request length validation

--- backend/medicine/views/medicine_starts_with.py
+++ backend/medicine/views/medicine_starts_with.py
@@ -9,12 +9,25 @@ from rest_framework.response import Response
 class MedicineStartsWith(APIView):
     """Filter by start letter"""
 
+    def check_query_parms_length(self, medicine_starts_with):
+        if len(medicine_starts_with) == 1:
+            return True
+        else:
+            return False
+
     def get(self, request):
         medicine_starts_with = request.query_params.get("medicine_starts_with")
 
-        if len(medicine_starts_with) > 1:
-            return Response({"message": f"request string length is {len(medicine_starts_with)}"})
-        else:    
+        check_query_params_length = self.check_query_parms_length(medicine_starts_with)
+
+        if check_query_params_length:
             medicine = MedicineModel.objects.filter(name__startswith=medicine_starts_with)
             serializer = MedicineModelSerializer(medicine, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "query_params": f"{medicine_starts_with}", 
+                "length": f"{len(medicine_starts_with)}",
+                "message": "query_params length should be 1"
+            })
+            
